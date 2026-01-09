b3a597e8284a9441367cfb3c36ad07294b0350ee
Bernd Strobel <strobelb69@gmail.com>
2026-01-09 17:00:44 +0100

    wip

--- install.sh
+++ install.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
-PREFIX=/usr
-INITDIR=${PREFIX}/lib/systemd/system
-INITDIR_UBUNTU=${PREFIX}/lib/systemd/system
-USERDIR=${PREFIX}/lib/sysusers.d
-TMPFDIR=${PREFIX}/lib/tmpfiles.d
-UDEVDIR=${PREFIX}/lib/udev/rules.d
-POLKDIR=${PREFIX}/share/polkit/rules.d
-MANDIR=${PREFIX}/share/man/man1
-ENVDIR=/etc/conf.d
+#set -x
 
 MYDIR=$(dirname $0)
+INITDIR=/etc/systemd/system
+UDEVDIR=/usr/lib/udev/rules.d
+POLKDIR=/usr/share/polkit-1/rules.d
+KODI_USERNAME=kodi
+KODI_USERHOME=/var/lib/kodi
 
 RM='sudo rm'
 INSTALL="sudo install -p"
@@ -25,66 +21,95 @@ if [[ $# > 0 && ${1} == -u ]]; then
 	INSTCMD=$DOUNINST
 fi
 
-ECHO=echo
+#ECHO=echo
 
-function udevrules {
-	MYFILE=${MYDIR}/$(arch)/udev/99-kodi.rules
+function handlefile {
 	case $INSTCMD in
 		$DOINST)
-			printf 'Installing %s\n' $FUNCNAME
-			$ECHO ${INSTALL_DATA} ${MYFILE}
+			printf 'Installing %s\n' ${1}
+			$ECHO ${INSTALL_DATA} ${2} ${3}
 			;;
 		$DOUNINST)
-			printf 'Uninstalling %s\n' $FUNCNAME
-			$ECHO ${RM} ${MYFILE}
+			printf 'Uninstalling %s\n' ${1}
+			$ECHO ${RM} ${3}/$(basename ${2})
 			;;
 	esac
+}
+
+function udevrules {
+	handlefile ${FUNCNAME} ${MYDIR}/$(arch)/udev/99-kodi.rules ${UDEVDIR}
 }
 
 function polkitrules {
-	MYFILE=${MYDIR}/common/polkit/polkit.rules
-	case $INSTCMD in
-		$DOINST)
-			printf 'Installing %s\n' $FUNCNAME
-			$ECHO ${INSTALL_DATA} ${MYFILE}
-			;;
-		$DOUNINST)
-			printf 'Uninstalling %s\n' $FUNCNAME
-			$ECHO ${RM} ${MYFILE}
-			;;
-	esac
+	# This kodi-polkit.rules files references account "kodi" by name!
+	handlefile ${FUNCNAME} ${MYDIR}/common/polkit/kodi-polkit.rules ${POLKDIR}
 }
 
-function systemd_sysusers {
+function kodiuser {
 	case $INSTCMD in
 		$DOINST)
 			printf 'Installing %s\n' $FUNCNAME
+			if $(id -u $KODI_USERNAME >/dev/null 2>/dev/null); then
+				printf 'User %s exists already. Doing nothing!\n'
+			else
+				$ECHO sudo useradd \
+					--home-dir $KODI_USERHOME \
+					--comment 'Kodi System User' \
+					--user-group \
+					--system \
+					--groups audio,video,dialout,input,cdrom,render \
+					--create-home \
+					$KODI_USERNAME
+			fi
 			;;
 		$DOUNINST)
 			printf 'Uninstalling %s\n' $FUNCNAME
-			;;
-	esac
-}
-
-function systemd_tmpfiles {
-	case $INSTCMD in
-		$DOINST)
-			printf 'Installing %s\n' $FUNCNAME
-			;;
-		$DOUNINST)
-			printf 'Uninstalling %s\n' $FUNCNAME
+			if $(id -u $KODI_USERNAME >/dev/null 2>/dev/null); then
+				$ECHO sudo userdel \
+					--force \
+					--remove \
+					$KODI_USERNAME
+			else
+				printf 'User %s does not exist. Doing nothing!\n' $KODI_USERNAME
+			fi
+			if $(id -g $KODI_USERNAME >/dev/null 2>/dev/null); then
+				$ECHO sudo groupdel \
+					--force \
+					$KODI_USERNAME
+			else
+				printf 'Group %s does not exist. Doing nothing!\n' $KODI_USERNAME
+			fi
 			;;
 	esac
 }
 
 function systemd_service {
-	# Note to self: Environment nicht vergessen!
 	case $INSTCMD in
 		$DOINST)
 			printf 'Installing %s\n' $FUNCNAME
+			handlefile ${FUNCNAME}_definition ${MYDIR}/common/init/kodi.service ${INITDIR}
+			$ECHO sudo systemctl daemon-reload
+			$ECHO sudo systemctl enable kodi.service
 			;;
 		$DOUNINST)
 			printf 'Uninstalling %s\n' $FUNCNAME
+			$ECHO sudo systemctl stop kodi.service
+			$ECHO sudo systemctl disable kodi.service
+			handlefile ${FUNCNAME}_definition ${MYDIR}/common/init/kodi.service ${INITDIR}
+			$ECHO sudo systemctl daemon-reload
+			;;
+	esac
+}
+
+function disable_graphical_login {
+	case $INSTCMD in
+		$DOINST)
+			printf 'Installing %s\n' $FUNCNAME
+			$ECHO sudo systemctl set-default multi-user.target
+			;;
+		$DOUNINST)
+			printf 'Uninstalling %s\n' $FUNCNAME
+			$ECHO sudo systemctl set-default graphical.target
 			;;
 	esac
 }
@@ -92,9 +117,9 @@ function systemd_service {
 install_tasks=( \
 udevrules \
 polkitrules \
-systemd_sysusers \
-systemd_tmpfiles \
+kodiuser \
 systemd_service \
+disable_graphical_login \
 )
 
 printf 'Doing the work...\n'
